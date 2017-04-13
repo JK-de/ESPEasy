403c1f960904c6bd3f44eb3455fb8d562eace966
JK <jkdesign@me.com>
2017-04-13 13:29:17 +0200

    work release

--- src/_P142_RGB-Strip.ino
+++ src/_P142_RGB-Strip.ino
@@ -15,8 +15,11 @@
 //#include <Adafruit_NeoPixel.h>
 //Adafruit_NeoPixel *Plugin_142_pixels;
 
-float Plugin_142_hsvAct[3] = {0,0,0};
-float Plugin_142_hsvDest[3] = {0,0,0};
+static float Plugin_142_hsvPrev[3] = {0,0,0};
+static float Plugin_142_hsvDest[3] = {0,0,0};
+static long millisFadeBegin = 0;
+static long millisFadeEnd = 0;
+static long millisFadeTime = 1000;
 
 #define PLUGIN_142
 #define PLUGIN_ID_142         142
@@ -100,12 +103,23 @@ boolean Plugin_142(byte function, struct EventStruct *event, String& string)
         String plugin5 = WebServer.arg("taskdeviceport");
         Settings.TaskDevicePluginConfig[event->TaskIndex][3] = plugin5.toInt();
 
+        for (byte i=0; i<4; i++)
+          if (Settings.TaskDevicePluginConfig[event->TaskIndex][3] >= 16)
+            Settings.TaskDevicePluginConfig[event->TaskIndex][3] = -1;
+
         success = true;
         break;
       }
 
     case PLUGIN_INIT:
       {
+        for (byte i=0; i<4; i++)
+        {
+          int pin = Settings.TaskDevicePluginConfig[event->TaskIndex][i];
+          if (pin >= 0)
+            pinMode(pin, OUTPUT);
+        }
+        Output(event->TaskIndex, Plugin_142_hsvDest);
         //if (!Plugin_142_pixels)
         {
           //Plugin_142_pixels = new Adafruit_NeoPixel(Settings.TaskDevicePluginConfig[event->TaskIndex][0], Settings.TaskDevicePin1[event->TaskIndex], NEO_GRB + NEO_KHZ800);
@@ -127,10 +141,9 @@ boolean Plugin_142(byte function, struct EventStruct *event, String& string)
           rgb[0] = event->Par1 / 255.0;   //R
           rgb[1] = event->Par2 / 255.0;   //G
           rgb[2] = event->Par3 / 255.0;   //B
-          clamp(rgb);
-          rgb2hsv(rgb, Plugin_142_hsvDest)
+          hsvClamp(rgb);
+          rgb2hsv(rgb, Plugin_142_hsvDest);
           bNewValue = true;
-          success = true;
         }
 
         if (command == F("hsv"))
@@ -138,20 +151,188 @@ boolean Plugin_142(byte function, struct EventStruct *event, String& string)
           Plugin_142_hsvDest[0] = event->Par1 / 360.0;   //Hue
           Plugin_142_hsvDest[1] = event->Par2 / 100.0;   //Saturation
           Plugin_142_hsvDest[2] = event->Par3 / 100.0;   //Value/Brightness
-          clamp(Plugin_142_hsvDest);
           bNewValue = true;
-          success = true;
+        }
+
+        if (command == F("hue"))
+        {
+          Plugin_142_hsvDest[0] = event->Par1 / 360.0;   //Hue
+          bNewValue = true;
+        }
+
+        if (command == F("dimm"))
+        {
+          Plugin_142_hsvDest[2] = event->Par1 / 100.0;   //Value/Brightness
+          bNewValue = true;
+        }
+
+        if (command == F("off"))
+        {
+          Plugin_142_hsvDest[2] = 0.0;   //Value/Brightness
+          bNewValue = true;
         }
 
         if (bNewValue)
         {
-          //analogWrite(event->Par1, event->Par2);
-          //setPinState(PLUGIN_ID_142, event->Par1, PIN_MODE_PWM, event->Par2);
+          hsvClamp(Plugin_142_hsvDest);
+          hsvClamp(Plugin_142_hsvPrev);
+
+          //get the shortest way around the color circle
+          if ((Plugin_142_hsvDest[0]-Plugin_142_hsvPrev[0]) > 0.5)
+            Plugin_142_hsvPrev[0] += 1.0;
+          if ((Plugin_142_hsvDest[0]-Plugin_142_hsvPrev[0]) < -0.5)
+            Plugin_142_hsvPrev[0] -= 1.0;
+
+          millisFadeBegin = millis();
+          millisFadeEnd = millisFadeBegin + millisFadeTime;
+
+          String log = F("hsv:  ");
+          for (byte i=0; i<3; i++)
+          {
+            log += toString(Plugin_142_hsvDest[i], 3);
+            log += F(" ");
+          }
+          addLog(LOG_LEVEL_INFO, log);
+
+          Output(event->TaskIndex, Plugin_142_hsvDest);
+          success = true;
         }
         break;
       }
+
+    case PLUGIN_READ:
+      {
+        UserVar[event->BaseVarIndex + 0] = Plugin_142_hsvDest[0] * 360.0;
+        UserVar[event->BaseVarIndex + 1] = Plugin_142_hsvDest[1] * 100.0;
+        UserVar[event->BaseVarIndex + 2] = Plugin_142_hsvDest[2] * 100.0;
+        success = true;
+
+        //String log = F("RGBW:  ");
+        //log += i;
+        //log += F(" bytes to attempt to fix buffer alignment");
+        //log += String(mhzResp[0], HEX);
+        //log += F(" ");
+        //addLog(LOG_LEVEL_ERROR, log);
+        //addLog(LOG_LEVEL_INFO, log);
+        break;
+      }
+
+    case PLUGIN_FIFTY_PER_SECOND:
+      break;
+      {
+        if (millisFadeEnd != 0)   //fading required?
+        {
+          float hsv[3];
+          long millisAct = millis();
+
+          if (millisAct >= millisFadeEnd)   //destination reached?
+          {
+            millisFadeBegin = 0;
+            millisFadeEnd = 0;
+            for (byte i=0; i<3; i++)
+              hsv[i] = Plugin_142_hsvPrev[i] = Plugin_142_hsvDest[i];
+          }
+          else   //just fading
+          {
+            float fade = (millisAct-millisFadeBegin) / (millisFadeEnd-millisFadeBegin);
+
+            fade = 1.0-fade;   //smooth fading out
+            fade *= fade;
+            fade = 1.0-fade;
+
+            for (byte i=0; i<3; i++)
+              hsv[i] = mix(Plugin_142_hsvPrev[i], Plugin_142_hsvDest[i], fade);
+          }
+
+          Output(event->TaskIndex, hsv);
+        }
+        success = true;
+        break;
+      }
+
+/*
+        #define PLUGIN_INIT_ALL                     1
+        #define PLUGIN_INIT                         2
+        #define PLUGIN_READ                         3
+        #define PLUGIN_ONCE_A_SECOND                4
+        #define PLUGIN_TEN_PER_SECOND               5
+        #define PLUGIN_DEVICE_ADD                   6
+        #define PLUGIN_EVENTLIST_ADD                7
+        #define PLUGIN_WEBFORM_SAVE                 8
+        #define PLUGIN_WEBFORM_LOAD                 9
+        #define PLUGIN_WEBFORM_SHOW_VALUES         10
+        #define PLUGIN_GET_DEVICENAME              11
+        #define PLUGIN_GET_DEVICEVALUENAMES        12
+        #define PLUGIN_WRITE                       13
+        #define PLUGIN_EVENT_OUT                   14
+        #define PLUGIN_WEBFORM_SHOW_CONFIG         15
+        #define PLUGIN_SERIAL_IN                   16
+        #define PLUGIN_UDP_IN                      17
+        #define PLUGIN_CLOCK_IN                    18
+        #define PLUGIN_TIMER_IN                    19
+        #define PLUGIN_FIFTY_PER_SECOND            20
+        #define PLUGIN_REMOTE_CONFIG               21
+        */
+
+
   }
   return success;
+}
+
+void Output(int TaskIndex, float* hsvIn)
+{
+  float hsvw[4];
+  float rgbw[4];
+
+  for (byte i=0; i<3; i++)
+    hsvw[i] = hsvIn[i];
+
+  hsvClamp(hsvw);
+
+  if (0)   //has white channel?
+  {
+    hsvw[3] = (1.0 - hsvw[1]) * hsvw[2];   // w = (1-s)*v
+    hsvw[2] *= hsvw[1];   // v = s*v
+  }
+  else
+    hsvw[3] = 0.0;
+
+
+  hsv2rgb(hsvw, rgbw);
+  rgbw[3] = hsvw[3];
+
+  int actRGBW[4];
+  static int lastRGBW[4] = {-1,-1,-1,-1};
+
+  String log = F("RGBW:  ");
+
+  for (byte i=0; i<4; i++)
+  {
+    int pin = Settings.TaskDevicePluginConfig[TaskIndex][i];
+    if (pin >= 0)
+    {
+      rgbw[i] *= rgbw[i];   //simple gamma correction
+
+      actRGBW[i] = rgbw[i] * PWMRANGE + 0.5;
+
+      log += String(rgbw[i], DEC);
+      log += F(" ");
+
+      if (Settings.TaskDevicePin1Inversed[TaskIndex])
+        actRGBW[i] = PWMRANGE - actRGBW[i];
+
+      if (lastRGBW[i] != actRGBW[i])
+      {
+        lastRGBW[i] = actRGBW[i];
+
+        analogWrite(pin, actRGBW[i]);
+        setPinState(PLUGIN_ID_142, pin, PIN_MODE_PWM, actRGBW[i]);
+      }
+    }
+  }
+
+  addLog(LOG_LEVEL_INFO, log);
+
 }
 
 // HSV->RGB conversion based on GLSL version
@@ -188,9 +369,14 @@ float* rgb2hsv(const float* rgb, float* hsv)
   return hsv;
 }
 
-float* clamp(float* hsv)
+float* hsvClamp(float* hsv)
 {
-  for (byte i=0; i<3; i++)
+  while (hsv[0] > 1.0)
+    hsv[0] -= 1.0;
+  while (hsv[0] < 0.0)
+    hsv[0] += 1.0;
+
+  for (byte i=1; i<3; i++)
   {
     if (hsv[i] < 0.0)
       hsv[i] = 0.0;
